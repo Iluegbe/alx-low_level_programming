9ad42a4c27ced1b94be06655cc8d46056870c101
iluegbe <udueboriluegbe@gmail.com>
2022-09-08 14:00:06 -0700

    Low level commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Serves as the entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
