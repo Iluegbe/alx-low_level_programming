0251fa8838b38c91d2a1afe73ffc32a7ec852d10
iluegbe <udueboriluegbe@gmail.com>
2022-09-09 22:03:38 -0700

    My variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry poimt
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+for (a = 'A'; a <= 'Z'; a++)
+putchar(a);
+putchar ('\n');
+return (0);
+}
